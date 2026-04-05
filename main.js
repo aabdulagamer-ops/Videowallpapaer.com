72ded6918121901714f76821332bfea760deb4b6
aabdulagamer-ops <aabdulagamer@gmail.com>
2026-04-05 15:48:19 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    title: "Video Wallpaper"
+  });
+
+  win.loadFile(path.join(__dirname, 'index.html'));
+
+  // Debug (important)
+  win.webContents.openDevTools();
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
